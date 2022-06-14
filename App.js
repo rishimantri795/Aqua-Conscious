ce97f45adeb8a8b7a2b5b63fd7b949ea21d9c6ad
batuCoding <106035918+batuCoding@users.noreply.github.com>
2022-06-14 13:34:36 -0400

    New start screen, kinda...
    With cooler looking text boxes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 import {TextInput, Dimensions } from "react-native-web";
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -10,6 +10,7 @@ const Flex = () => {
  
     <View style={styles.container}>
 
+
      
 <LinearGradient
   colors={['rgba(1,30,243,0.8)', 'transparent']}
@@ -18,29 +19,47 @@ const Flex = () => {
     <Text style = {styles.tex}>Sign Up - Aqua Concious</Text>
     <Text style = {styles.texsml}>Name</Text>
 
+    <LinearGradient
+    colors={['rgba(96, 247, 255, 0.8)', 'rgba(2, 100, 105, 0.8)']} style = {styles.Border}>
     <TextInput style = {styles.item} placeholder = 'First Name - Last Name'/>
+    </LinearGradient>
 
     <Text style = {styles.texsml}>Number of people in your house</Text>
 
+    <LinearGradient
+    colors={['rgba(96, 247, 255, 0.8)', 'rgba(2, 100, 105, 0.8)']} style = {styles.Border}>
     <TextInput style = {styles.item} placeholder = 'Type Here'/>
+    </LinearGradient>
 
     <Text style = {styles.texsml}>Water Provider</Text>
 
+    <LinearGradient
+    colors={['rgba(96, 247, 255, 0.8)', 'rgba(2, 100, 105, 0.8)']} style = {styles.Border}>
     <TextInput style = {styles.item} placeholder = 'Name of company'/>
+    </LinearGradient>
 
     <Text style = {styles.texsml}>Zipcode</Text>
 
+    <LinearGradient
+    colors={['rgba(96, 247, 255, 0.8)', 'rgba(2, 100, 105, 0.8)']} style = {styles.Border}>
     <TextInput style = {styles.item} placeholder = 'XXXXXX'/>
+    </LinearGradient>
 
     <View  style={styles.separator}/>
 
     <Text style = {styles.texsml}>Email address</Text>
 
+    <LinearGradient
+    colors={['rgba(96, 247, 255, 0.8)', 'rgba(2, 100, 105, 0.8)']} style = {styles.Border}>
     <TextInput style = {styles.item} placeholder = 'exampleEmail@example.com'/>
+    </LinearGradient>
 
     <Text style = {styles.texsml}>Password</Text>
 
+    <LinearGradient
+    colors={['rgba(96, 247, 255, 0.8)', 'rgba(2, 100, 105, 0.8)']} style = {styles.Border}>
     <TextInput style = {styles.item} placeholder = 'PassWord1234$#AB'/>
+    </LinearGradient>
 
 <View style={styles.buttonFormat}>
     <Button
@@ -69,15 +88,19 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightblue',
     flexDirection: 'row',
   },
-
+  Border: {
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignContent: 'center',
+    textAlign: 'center',
+    marginHorizontal: win.width/10,
+  },
   item: {
     borderRadius: 15,
     padding: 20,
     justifyContent: 'center',
     alignContent: 'center',
     textAlign: 'center',
-    backgroundColor: 'rgba(190, 237,239, 0.8)', 
-    marginHorizontal: win.width/10,
   },
   tex: {
     borderRadius: 8,
@@ -104,7 +127,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     top: 0,
-    height: win.height/2,
+    height: win.height,
   },
   buttonFormat: {
     borderRadius: 15,
